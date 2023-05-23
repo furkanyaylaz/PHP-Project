1f96cdf62078f11f56a917293161983860df67f1
Furkan Yaylaz <92256027+furkanyaylaz@users.noreply.github.com>
2023-05-23 23:24:11 +0300

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ session_start();
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-// Kullanıcı adı ve şifreyi veritabanında kontrol et
+// Kullanıcı adı ve şifreyi veritabanında kontrol etme
 $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
 $result = $conn->query($sql);
 
