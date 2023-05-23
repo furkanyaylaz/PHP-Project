c6772aaf8aaec2b3b75889598363adb042950dcc
Furkan Yaylaz <92256027+furkanyaylaz@users.noreply.github.com>
2023-05-23 23:21:21 +0300

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $password = "Jtx7DjtRhh1A2";
 $dbname = "epiz_34270908_manav_siparis_sistemi";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-mysqli_set_charset($connection, "utf8mb4");
+mysqli_set_charset($conn, "utf8mb4");
 
 if (!$conn) {
     echo "MySQL sunucu ile baglanti kurulamadi! </br>";
